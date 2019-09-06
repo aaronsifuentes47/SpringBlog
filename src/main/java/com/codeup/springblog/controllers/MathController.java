922921d86a095db86432be20396910d3e8ae4335
aaronsifuentes47 <sifuentes.aaronm@gmail.com>
2019-09-06 09:09:09 -0500

    first spring exercise

---
+++ src/main/java/com/codeup/springblog/controllers/MathController.java
@@ -0,0 +1,36 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class MathController {
+
+    @GetMapping("/add/{num1}/and/{num2}")
+    @ResponseBody
+    public String doAdd(@PathVariable int num1, @PathVariable int num2) {
+        return "" + num1 + " plus " + num2 + " is " + (num1 + num2) + "!";
+
+    }
+    @GetMapping("/subtract/{num1}/and/{num2}")
+    @ResponseBody
+    public String doSub(@PathVariable int num1, @PathVariable int num2) {
+        return "" + num1 + " minus " + num2 + " is " + (num1 - num2) + "!";
+
+    }
+    @GetMapping("/multiply/{num1}/and/{num2}")
+    @ResponseBody
+    public String doMult(@PathVariable int num1, @PathVariable int num2) {
+        return "" + num1 + " times " + num2 + " is " + (num1 * num2) + "!";
+
+    }
+    @GetMapping("/divide/{num1}/and/{num2}")
+    @ResponseBody
+    public String doDiv(@PathVariable int num1, @PathVariable int num2) {
+        return "" + num1 + " divided by " + num2 + " is " + (num1 / num2) + "!";
+
+    }
+
+}
---
+++ src/main/java/com/codeup/springblog/controllers/PostController.java
@@ -0,0 +1,33 @@
+package com.codeup.springblog.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class PostController {
+
+
+    @RequestMapping(path = "/posts", method = RequestMethod.GET)
+    @ResponseBody
+    public String index() {
+        return "Index page!";
+    }
+    @RequestMapping(path = "/posts/{id}", method = RequestMethod.GET)
+    @ResponseBody
+    public String individual(@PathVariable int id) {
+        return "individual post page of id #" + id;
+    }
+    @RequestMapping(path = "/posts/create", method = RequestMethod.GET)
+    @ResponseBody
+    public String viewCreate() {
+        return "view create page!";
+    }
+    @RequestMapping(path = "/posts/create", method = RequestMethod.POST)
+    @ResponseBody
+    public String actualCreate() {
+        return "create post!";
+    }
+}
