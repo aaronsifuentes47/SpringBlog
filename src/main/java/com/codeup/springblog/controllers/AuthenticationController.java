b1615c74e88b5992b2e4d65c8f3c42651706376e
aaronsifuentes47 <sifuentes.aaronm@gmail.com>
2019-09-16 17:30:02 -0500

    fixed login and profile error

--- src/main/java/com/codeup/springblog/controllers/AuthenticationController.java
+++ src/main/java/com/codeup/springblog/controllers/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.codeup.springblog.controllers;
 
+        import com.codeup.springblog.models.User;
+        import org.springframework.security.core.context.SecurityContextHolder;
         import org.springframework.stereotype.Controller;
         import org.springframework.web.bind.annotation.RequestMapping;
         import org.springframework.web.bind.annotation.RequestMethod;
@@ -9,6 +11,12 @@ public class AuthenticationController {
 
     @RequestMapping(path = "/login", method = RequestMethod.GET)
     public String showLoginForm() {
+        try {
+            User userSession = (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            return "redirect:/profile";
+        } catch (Exception e){
+            e.printStackTrace();
         return "users/login";
+        }
     }
 }
